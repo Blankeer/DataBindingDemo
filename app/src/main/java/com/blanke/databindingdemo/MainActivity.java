1cee21ce75c9ed1f5b91867c3aba5913cce09800
Blanke <blankeeee@gmail.com>
2017-11-16 14:25:16 +0800

    [ADD] add simple data binding project.

---
+++ app/src/main/java/com/blanke/databindingdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.blanke.databindingdemo;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blanke.databindingdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setNews(new News("title...", "content....."));
+    }
+}
